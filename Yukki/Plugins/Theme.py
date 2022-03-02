b98c0ceb41a7a6c6f634a8d0a5c49168e04a0aee
Santhosh podili <99010597+Santhupodili@users.noreply.github.com>
2022-03-02 19:49:10 +0530

    Update Theme.py

--- Yukki/Plugins/Theme.py
+++ Yukki/Plugins/Theme.py
@@ -34,10 +34,10 @@ __HELP__ = """
 
 
 /settheme
-- Set a theme for thumbnails.
+- sᴇᴛ ᴀ ᴛʜᴇᴍᴇ ғᴏʀ ᴛʜᴜᴍʙɴᴀɪʟs.
 
 /theme
-- Check Theme for your chat.
+- ᴄʜᴇᴄᴋ ᴛʜᴇᴍᴇ ғᴏʀ ʏᴏᴜʀ ᴄʜᴀᴛ.
 """
 
 
@@ -45,7 +45,7 @@ __HELP__ = """
     filters.command(["settheme", f"settheme@{BOT_USERNAME}"]) & filters.group
 )
 async def settheme(_, message):
-    usage = f"This isn't a theme.\n\nSelect from them\n{' | '.join(themes)}\n\nUse 'Random' to get random choice of themes"
+    usage = f"This isn't a theme.\n\nsᴇʟᴇᴄᴛ ғʀᴏᴍ ᴛʜᴇᴍ\n{' | '.join(themes)}\n\nUse 'Random' ᴛᴏ ɢᴇᴛ ʀᴀɴᴅᴏᴍ ᴄʜᴏɪᴄᴇ ᴏғ ᴛʜᴇᴍᴇs"
     if len(message.command) != 2:
         return await message.reply_text(usage)
     theme = message.text.split(None, 1)[1].strip()
@@ -55,7 +55,7 @@ async def settheme(_, message):
         "theme": theme,
     }
     await save_theme(message.chat.id, "theme", note)
-    await message.reply_text(f"Changed thumbnail theme to {theme}")
+    await message.reply_text(f"ᴄʜᴀɴɢᴇᴅ ᴛʜᴜᴍʙɴᴀɪʟ ᴛʜᴇᴍᴇ ᴛᴏ {theme}")
 
 
 @app.on_message(filters.command("theme"))
